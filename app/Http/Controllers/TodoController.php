1a9796c76d8b9999f147fb8914735180ca4b7f37
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-03-27 20:15:33 +0700

    migration, database seeder, factory

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,13 +1,27 @@
 <?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class TodoController extends Controller
-{
-    public function view(){
-       return view('todo.index'); 
-    }
-
-}
+ 
+ namespace App\Http\Controllers;
+ 
+ use Illuminate\Http\Request;
+ use Illuminate\Support\Facades\Auth;
+ use App\Models\Todo;
+ 
+ class TodoController extends Controller
+ {
+     public function index()
+     {
+         $todos = Todo::all();
+         // $todos = Todo::where('user_id', Auth::id())->get();
+         dd($todos);
+         return view("todo.index");
+     }
+ 
+     public function edit()
+     {
+         return view("todo.edit");
+     }
+     public function create()
+     {
+         return view("todo.create");
+     }
+ }
