8abe89f49540e36fcc82944694566d3ad30f0119
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-05-02 20:42:41 +0700

    update, delete data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,24 +9,24 @@ class UserController extends Controller
 {
     public function index()
 {
-    // $users = User::where('id', '!=', 1)->orderBy('name')->paginate(10);
-    // return view('user.index', compact('users'));
-    $search = request('search');
-    if ($search) {
-        $users = User::where(function ($query) use ($search) {
-            $query->where('name', 'like', '%' . $search . '%')
-                  ->orWhere('email', 'like', '%' . $search . '%');
-        })
-        ->orderBy('name')
-        ->where('id', '!=', 1)
-        ->paginate(20)
-        ->withQueryString();
-    } else {
-        $users = User::where('id', '!=', 1)
-                     ->orderBy('name')
-                     ->paginate(20);
+    {
+        $search = request('search');
+
+        $query = User::query()
+            ->where('id', '!=', 1)
+            ->orderBy('name');
+
+        if ($search) {
+            $query->where(function ($q) use ($search) {
+                $q->where('name', 'like', "%{$search}%")
+                  ->orWhere('email', 'like', "%{$search}%");
+            });
+        }
+
+        $users = $query->paginate(20)->withQueryString();
+
+        return view('user.index', compact('users'));
     }
-    return view('user.index', compact('users'));
 }
     public function create()
     {
@@ -36,5 +36,38 @@ class UserController extends Controller
     public function edit()
     {
         return view('user.edit');
+    }
+    public function makeadmin(User $user)
+     {
+         if ($user->id != 1) {
+             $user->is_admin = true;
+             $user->save();
+ 
+             return back()->with('success', 'Make admin successfully!');
+         }
+ 
+         return redirect()->route('user.index');
+     }
+ 
+     public function removeadmin(User $user)
+     {
+         if ($user->id != 1) {
+             $user->is_admin = false;
+             $user->save();
+ 
+             return back()->with('success', 'Remove admin successfully!');
+         }
+ 
+         return redirect()->route('user.index');
+     }
+ 
+     public function destroy(User $user)
+ {
+     if ($user->id != 1) {
+         $user->delete();
+         return back()->with('success', 'User deleted successfully!');
+     } else {
+         return redirect()->route('user.index')->with('danger', 'Cannot delete this user!');
+     }
     }
 }
---
+++ tugas6.md
@@ -0,0 +1,33 @@
+# Tugas 6
+
+ ## Todo Edited
+ ![alt text](screenshot/tugas6/image1.png)
+
+ ##  Todo Completed
+![alt text](image.png)
+
+ ##  Todo Delete All Completed Todo
+ ![alt text](screenshot/tugas6/image4.png)
+
+ ##  Todo Deleted
+ ![alt text](screenshot/tugas6/image5.png)
+ 
+##  Edit Todo
+ ![alt text](screenshot/tugas6/image6.png)
+ ![alt text](screenshot/tugas6/image7.png)
+
+
+ ##  Todo Uncompleted
+ ![alt text](screenshot/tugas6/image8.png)
+ 
+ 
+  
+ ##  User Deleted
+ ![alt text](screenshot/tugas6/image9.png)
+ 
+
+ ##  User Make Admin
+ ![alt text](screenshot/tugas6/image10.png)
+ 
+ ##  User Remove Admin
+ ![alt text](screenshot/tugas6/image11.png)
